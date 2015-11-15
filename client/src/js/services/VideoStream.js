52a2b2aefa58da01fb9486e176629b8c0bd24e79
Łukasz Sałajczyk <lukasz.salajczyk@jazzy.pro>
2015-11-15 21:31:00 +0100

    Private scope for VideoStream class

--- client/src/js/services/VideoStream.js
+++ client/src/js/services/VideoStream.js
@@ -1,8 +1,122 @@
 var VideoStream = (function(){
   'use strict';
 
-  // @todo Miganie
-  // @todo _private scope
+  var _private = {
+    /**
+     * @param {Object} elements
+     */
+    init: function(elements){
+      _private.storeElements.call(this, elements);
+
+      _private.createVideoStream.call(this);
+      _private.createCanvas.call(this);
+
+      setInterval(_private.drawFrameOnCanvas.bind(this), 1000 / 24);
+      setInterval(this.onProcess.bind(this, this.elems, this.ctx), 200);
+    },
+
+    /**
+     * @param {MediaStreamConstaints} constraints
+     * @param {Function} successCallback
+     * @param {Function} errorCallback
+     * @returns {Function}
+     */
+    getUserMedia: function(constraints, successCallback, errorCallback){
+      var n = navigator;
+
+      if(n.getUserMedia){
+        return n.getUserMedia.apply(n, arguments);
+      }
+      if(n.webkitGetUserMedia){
+        return n.webkitGetUserMedia.apply(n, arguments);
+      }
+      if(n.mozGetUserMedia){
+        return n.mozGetUserMedia.apply(n, arguments);
+      }
+      if(n.msGetUserMedia){
+        return n.msGetUserMedia.apply(n, arguments);
+      }
+
+      return null;
+    },
+
+    /**
+     *
+     * @returns {Boolean}
+     */
+    hasUserMedia: function(){
+      var n = navigator;
+
+      return  !!(n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia);
+    },
+
+    /**
+     * @param {Object} elements
+     */
+    storeElements: function(elements){
+      this.elems = {
+        video:  elements.video,
+        canvas: elements.canvas
+      };
+
+      this.ctx = this.elems.canvas.getContext('2d');
+    },
+
+    /**
+     *
+     */
+    createVideoStream: function(){
+      if(false === _private.hasUserMedia.call(this)){
+        console.log('Your browser has`t got navigator.getUserMedia. Try in Chrome');
+        return;
+      }
+
+      _private.getUserMedia.call(
+        this,
+        {
+          video: {
+            mandatory: {
+              maxWidth:   this.size.w,
+              maxHeight:  this.size.h
+            }
+          }
+        },
+        _private.onVideoStream.bind(this),
+        _private.onVideoStreamError.bind(this)
+      );
+    },
+
+    /**
+     * @param {MediaStream} stream
+     */
+    onVideoStream: function(stream){
+      this.elems.video.src = window.URL.createObjectURL(stream);
+    },
+
+    /**
+     *
+     */
+    onVideoStreamError: function(){
+      console.warn('Video stream error');
+    },
+
+    /**
+     *
+     */
+    createCanvas: function(){
+      this.elems.canvas.setAttribute('width', this.size.w);
+      this.elems.canvas.setAttribute('height', this.size.h);
+    },
+
+    /**
+     *
+     */
+    drawFrameOnCanvas: function(){
+      this.ctx.drawImage(this.elems.video, 0, 0);
+
+      this.onDrawFrame.call(this, this.ctx);
+    }
+  };
 
   /**
    *
@@ -12,156 +126,40 @@ var VideoStream = (function(){
    * @param {Function} onDrawFrame
    * @constructor
    */
-  var VideoStreamClass = function(elements, size, onProcess, onDrawFrame){
-    /**
-     *
-     * @type {Object}
-     */
-    this.elems  = {};
+  return function(elements, size, onProcess, onDrawFrame){
+      /**
+       *
+       * @type {Object}
+       */
+      this.elems  = {};
 
-    /**
-     *
-     * @type {null}
-     */
-    this.ctx    = null;
+      /**
+       *
+       * @type {null}
+       */
+      this.ctx    = null;
 
-    /**
-     *
-     * @type {Object}
-     */
-    this.size   = {
-      w: size.w || 480,
-      h: size.h || 320
+      /**
+       *
+       * @type {Object}
+       */
+      this.size   = {
+        w: size.w || 480,
+        h: size.h || 320
+      };
+
+      /**
+       *
+       * @type {Function}
+       */
+      this.onDrawFrame = onDrawFrame || function(){};
+
+      /**
+       *
+       * @type {Function}
+       */
+      this.onProcess = onProcess || function(){};
+
+      _private.init.call(this, elements);
     };
-
-    /**
-     *
-     * @type {Function}
-     */
-    this.onDrawFrame = onDrawFrame || function(){};
-
-    /**
-     *
-     * @type {Function}
-     */
-    this.onProcess = onProcess || function(){};
-
-    this.init(elements);
-  };
-
-  /**
-   * @param {Object} elements
-   */
-  VideoStreamClass.prototype.init = function(elements){
-    this.storeElements(elements);
-
-    this.createVideoStream();
-    this.createCanvas();
-
-    setInterval(this.drawFrameOnCanvas.bind(this), 1000 / 24);
-    setInterval(this.onProcess.bind(this, this.elems, this.ctx), 200);
-  };
-
-  /**
-   * @param {MediaStreamConstaints} constraints
-   * @param {Function} successCallback
-   * @param {Function} errorCallback
-   * @returns {Function}
-   */
-  VideoStreamClass.prototype.getUserMedia = function(constraints, successCallback, errorCallback){
-    var n = navigator;
-    
-    if(n.getUserMedia){
-      return n.getUserMedia.apply(n, arguments);
-    }
-    if(n.webkitGetUserMedia){
-      return n.webkitGetUserMedia.apply(n, arguments);
-    }
-    if(n.mozGetUserMedia){
-      return n.mozGetUserMedia.apply(n, arguments);
-    }
-    if(n.msGetUserMedia){
-      return n.msGetUserMedia.apply(n, arguments);
-    }
-
-    return null;
-  };
-
-  /**
-   *
-   * @returns {Boolean}
-   */
-  VideoStreamClass.prototype.hasUserMedia = function(){
-    var n = navigator;
-
-    return  !!(n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia);
-  };
-
-  /**
-   * @param {Object} elements
-   */
-  VideoStreamClass.prototype.storeElements = function(elements){
-    this.elems = {
-      video:  elements.video,
-      canvas: elements.canvas
-    };
-
-    this.ctx = this.elems.canvas.getContext('2d');
-  };
-
-  /**
-   *
-   */
-  VideoStreamClass.prototype.createVideoStream = function(){
-    if(false === this.hasUserMedia()){
-      console.log('Your browser has`t got navigator.getUserMedia. Try in Chrome');
-      return;
-    }
-
-    this.getUserMedia(
-      {
-        video: {
-          mandatory: {
-            maxWidth:   this.size.w,
-            maxHeight:  this.size.h
-          }
-        }
-      },
-      this.onVideoStream.bind(this),
-      this.onVideoStreamError.bind(this)
-    );
-  };
-
-  /**
-   * @param {MediaStream} stream
-   */
-  VideoStreamClass.prototype.onVideoStream = function(stream){
-    this.elems.video.src = window.URL.createObjectURL(stream);
-  };
-
-  /**
-   *
-   */
-  VideoStreamClass.prototype.onVideoStreamError = function(){
-    console.warn('Video stream error');
-  };
-
-  /**
-   *
-   */
-  VideoStreamClass.prototype.createCanvas = function(){
-    this.elems.canvas.setAttribute('width', this.size.w);
-    this.elems.canvas.setAttribute('height', this.size.h);
-  };
-
-  /**
-   *
-   */
-  VideoStreamClass.prototype.drawFrameOnCanvas = function(){
-    this.ctx.drawImage(this.elems.video, 0, 0);
-
-    this.onDrawFrame.call(this, this.ctx);
-  };
-
-  return VideoStreamClass;
 }());
